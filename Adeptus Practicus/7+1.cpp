cce574f75ea1b7c97008b45d738ef88a8a9bc10e
Zhulega Igor <zhylega2001@mail.ru>
2020-07-02 17:37:47 +0300

    Отредактировал помарки в денвнике

---
+++ Adeptus Practicus/7+1.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <stdio.h>
+# define N 5
+
+using namespace std;
+
+// Вариант с считыванием из файла
+void sfile(int mas[][N]) {
+	FILE* f;
+	fopen_s(&f, "1.txt", "r");
+
+	if (f) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				fscanf_s(f, "%d", &mas[i][j]);
+			}
+		}
+		fclose(f);
+	}
+
+}
+
+
+void vruch(int mas[][N]) {
+
+	cout << "Введите эл. матрицы A(5 x 5):" << endl;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << "A[" << i << "][" << j << "]:\t";
+			cin >> mas[i][j];
+		}
+	}
+
+}
+
+int *vvod_X(int mas[][N], int X[N],void (*pfunc) (int masp[][N])) {
+
+	pfunc(mas);
+
+	int b=0, m=0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N - 1; j++) {
+			if (mas[i][j] < mas[i][j + 1]) b++; // по возростанию
+			else if (mas[i][j] > mas[i][j + 1]) m++; //по убыванию
+		}
+	}
+	if ((m == 20) or (b == 20))
+		for (int i = 0; i < N; i++) X[i] = 1;
+	else for (int i = 0; i < N; i++) X[i] = -1;
+	
+	return X;
+}
+
+
+void out(int X[], int mas[][N]) {
+
+	int i, j, max = -100000, min = 1000000 ;
+
+
+	cout << "Массив A(5 x 5): " << endl;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			cout << mas[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+	cout << "Массив X[5]:" << endl;
+
+	for (i = 0; i < N; i++) cout << X[i] << "\t";
+
+	for (i = 0; i < N; i++) {
+		if (mas[i][i] < min) min = mas[i][i];
+		else if (mas[i][i] > max) max = mas[i][i];
+	}
+
+	cout << endl << "Y являющийся средним арифм. главной диагонали равен: " << (max + min) / 2 << endl;
+
+}
+
+
+void main() {
+	setlocale(LC_CTYPE, "rus");
+
+	int c;
+
+	int mas[N][N];
+	int X[N];
+
+	void (*PointX)(int mas[][N]);
+	cout << "Вы хотите ввести матрицу в ручную (1-да\\другая цифра - нет)" << endl;
+
+	cin >> c;
+
+	switch(c) {
+	case 1: PointX = &vruch; break;
+
+	default: PointX = &sfile;
+	}
+
+
+	out(vvod_X(mas, X, PointX), mas);
+
+
+}
+
---
+++ Adeptus Practicus/9(а)+1.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <clocale>
+
+using namespace std;
+
+void polozhit(const int n, int* mas[]) {
+	
+	int t = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (mas[i][j] > 0) t++;
+		}
+	}
+
+	if (t == 0) cout << "Нет положительных эл." << endl;
+}
+
+void p1(int k, const int n, int m[], int* mas[]) {
+	int l, t = 0, c = 0;
+	for (int i = 0; i < n; i++) {
+		if (m[i] != (-1)) {
+			k++;
+			
+			t = 0;
+			if (k > 1) {
+				if (c == 0) {
+					cout << "Меняем столбец " << (i + 1) << " обнуляя" << endl;
+					c++;
+				}
+				for (int j = 0; j < n; j++) {
+					mas[j][i] = 0;
+				}
+			}
+		}//Обнуление повторяющихся (см. на условие варианта)
+		c = 0;
+	}
+}
+
+void kol_vo(int &i, const int n,  int m[], int* mas[]) {
+	int  l = n - 1, kol = 0;
+	
+	for (int j = 0; j < n; j++) {
+		while ((l >= 0) && (j!=l)) {
+			if (mas[j][i] == mas[l][i]) {
+				m[i] = i;//запомнил столбец с повт. эл.
+				
+				kol++;
+			}
+			l--;
+		}
+	}
+		cout << "Одинаковое кол-во эл. в столбце " << (i + 1) << " равняется: " << kol << endl;
+		kol = 0;
+}
+
+void raz(const int n, int* mas[]) {
+	int l, u;
+	int* m = new int[n];
+
+	
+	for (int i = 0; i < n; i++) {
+
+		polozhit(n, mas);
+		m[i] = -1;
+		int c = 0;
+
+		kol_vo(i, n, m, mas);
+
+		for (int j = n - 1; j >= 0; j--) {
+			l = j - 1;
+			while (l >= 0) {
+				
+				if (mas[j][i] < mas[l][i]) {
+					if (c == 0) {
+						cout << "Меняем столбец " << (i+1) << " по возростанию" << endl;
+						c++;
+					}
+					u = mas[j][i];
+					mas[j][i] = mas[l][i];
+					mas[l][i] = u;
+				}
+				l--;
+			}
+			
+			
+		}
+		
+	 c = 0;
+	}// Расспределение по возростанию эл. и запоминание столбцов с повт.
+
+	p1(0, n, m, mas);
+
+	delete[]m;
+
+}
+
+void p2(const int n, int* mas[]) {
+
+	cout << "После выполнения:" << endl;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << mas[i][j] << "\t";
+		}
+
+		cout << endl;
+	}//Вывод массива
+}
+
+void main() {
+
+	setlocale(LC_CTYPE, "rus");
+	cout << "Введите кол-во элементов n для A(n x n): " << endl;
+
+	int n, i, j, err = 0;
+
+	cin >> n;
+	int** mas = new int* [n];
+
+	for (i = 0; i < n; i++) {
+		mas[i] = new int[n];
+	}
+	//////////////////////////////////////////////////////////////////////
+
+	cout << "Введите элементы не равные 0:,\n";
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			cin >> mas[i][j];
+			if (mas[i][j] == 0)  break;
+		}
+		if (mas[i][j] == 0) {
+			cout << "Ошибка, найден элемент не удовлетворяющий условию!" << endl;
+			err = 1;
+			break; }
+	}// заполнение двумерного массива
+
+	
+
+
+	if (err != 1) {
+
+		raz(n, mas);
+
+		p2(n, mas);
+
+	}
+	
+
+
+	///////////////////////////////////////////////////////////////////////
+	for (i = 0; i < n; i++) {
+		delete[] mas[i];
+	}// Удалил одномерные массивы
+
+	delete[]mas;// Удалил динамический массив
+
+}
